52e274dfff5248c645abf0aac37439122bac7afb
Griffenholm <nicolinedh@gmail.com>
2024-12-11 11:13:54 +0100

    Maven pom file burde være helt klar til at køre. I main har jeg tilføjet en kode der som det første skal sørge for at apien kører. Jeg har tilføjet en resources folder hvor min json api key ligger. For at api key ikke er tilgængelig for alle på nettet (github), har jeg tilføjet en fil der hedder .gitignore og lagt stien ind for api key. Niki og Mathias skal selv indsætte json filen med key i resourses. Jeg sender den.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
+        // Path to the service account JSON key google vision API
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "src/main/resources/service-account-api-key.json");
+
         TextileService s = new TextileService("TextileHouse");
         s.startTextileService();
 
