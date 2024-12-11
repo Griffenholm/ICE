837b24a5ffc0e052d55975224abf6bba720b1a80
Griffenholm <nicolinedh@gmail.com>
2024-12-11 09:37:25 +0100

    Notes in fabricfinder class pom file has been updated

---
+++ src/main/java/FabricFinder.java
@@ -0,0 +1,24 @@
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FabricFinder {
+
+    //I have installed the plugin Google Cloud Code to intelliJ.
+    // User can upload a picture to the console. She writes the path of the file.
+    //Picture is sent to an API which will return with the RGB color profile.
+    //Our code will tell which color it is. Red or blue (to start with), and return with the names of the fabrics from our store
+    //User get similar product results that she can click on and go to product details. textile.csv
+    //(Text is generated from the picture, to compare with our pictures.  https://api-ninjas.com/api/imagetotext)
+
+
+
+
+
+}
